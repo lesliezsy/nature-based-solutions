4fb001a65a6704af8c643d554b8add97ca9859bb
Leslie <lesliezsy12@gmail.com>
2025-05-27 13:21:03 -0700

    Add base path

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // base: process.env.VITE_BASE_PATH || "/",
+  base: process.env.VITE_BASE_PATH || "/nature-based-solutions",
 });
 
